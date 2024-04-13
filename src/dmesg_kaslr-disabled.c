f3f1b4f3094a5566f2fea7c0d2859018dfb6f8da
bcoles <bcoles@gmail.com>
2024-04-13 17:05:19 +1000

    dmesg_kaslr-disabled: Search for 'KASLR is disabled' used by LoongArch

--- src/dmesg_kaslr-disabled.c
+++ src/dmesg_kaslr-disabled.c
@@ -4,10 +4,15 @@
 //
 // x86/x86_64:
 // KASLR disabled: 'kaslr' not on cmdline (hibernation selected).
+// KASLR disabled: 'nokaslr' on cmdline.
 //
 // ARM64:
+// KASLR disabled on command line
 // KASLR disabled due to lack of seed
 // KASLR disabled due to FDT remapping failure
+//
+// LoongArch:
+// KASLR is disabled.
 //
 // S390:
 // KASLR disabled: CPU has no PRNG
@@ -26,6 +31,7 @@
 // https://elixir.bootlin.com/linux/v6.1.6/source/arch/arm64/kernel/kaslr.c#L45
 // https://elixir.bootlin.com/linux/v6.1.1/source/arch/s390/boot/kaslr.c#L35
 // https://elixir.bootlin.com/linux/v6.1.1/source/arch/s390/boot/kaslr.c#L201
+// https://elixir.bootlin.com/linux/v6.8.5/source/arch/loongarch/kernel/relocate.c#L107
 // ---
 // <bcoles@gmail.com>
 
@@ -34,46 +40,50 @@
 #include "include/kasld.h"
 #include <errno.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 unsigned long search_dmesg_kaslr_disabled() {
   int size;
-  char *syslog;
-  char *line;
+  char *syslog = 0;
+  char *line = 0;
   const char *needle = "KASLR disabled";
+  const char *needle2 = "KASLR is disabled";
   bool nokaslr = false;
 
-  printf("[.] searching dmesg for '%s' ...\n", needle);
+  printf("[.] searching dmesg for '%s' or '%s' ...\n", needle, needle2);
 
   if (mmap_syslog(&syslog, &size))
     return 0;
 
   line = strtok(syslog, "\n");
   while ((line = strtok(NULL, "\n")) != NULL) {
-    if (strstr(line, needle)) {
-      nokaslr = true;
-      printf("[.] Kernel was booted with KASLR disabled\n");
+    if (strstr(line, needle) || strstr(line, needle2)) {
       // printf("%s\n", line);
+      nokaslr = true;
       break;
     }
   }
 
-  if (nokaslr)
+  if (nokaslr) {
+    printf("[.] Kernel was booted with KASLR disabled\n");
     return (unsigned long)KERNEL_TEXT_DEFAULT;
+  }
 
   return 0;
 }
 
 unsigned long search_dmesg_log_file_kaslr_disabled() {
   FILE *f;
-  char *line;
+  char *line = 0;
   size_t size = 0;
   const char *path = "/var/log/dmesg";
   const char *needle = "KASLR disabled";
+  const char *needle2 = "KASLR is disabled";
   bool nokaslr = false;
 
-  printf("[.] searching %s for '%s' ...\n", path, needle);
+  printf("[.] searching %s for '%s' or '%s' ...\n", path, needle, needle2);
 
   f = fopen(path, "rb");
 
@@ -83,18 +93,20 @@ unsigned long search_dmesg_log_file_kaslr_disabled() {
   }
 
   while ((getline(&line, &size, f)) != -1) {
-    if (strstr(line, needle)) {
-      nokaslr = true;
-      printf("[.] Kernel was booted with KASLR disabled\n");
+    if (strstr(line, needle) || strstr(line, needle2)) {
       // printf("%s\n", line);
+      nokaslr = true;
       break;
     }
   }
 
+  free(line);
   fclose(f);
 
-  if (nokaslr)
+  if (nokaslr) {
+    printf("[.] Kernel was booted with KASLR disabled\n");
     return (unsigned long)KERNEL_TEXT_DEFAULT;
+  }
 
   return 0;
 }
